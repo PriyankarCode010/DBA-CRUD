90e136031bc69ee747edc5eecd90ea0307c1864b
sirdesai22 <sirdesai.work@gmail.com>
2025-03-09 14:57:48 +0530

    Tweet - edit, delete methods added

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,39 +1,34 @@
 "use client";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardHeader } from "@/components/ui/card"
-import { Label } from "@/components/ui/label"
-import { Separator } from "@/components/ui/separator"
-import { ChevronLeft, Edit, Lock } from "lucide-react"
-import Link from "next/link"
-import { Sidebar } from "@/components/SideBar"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Sidebar } from "@/components/SideBar";
 import { auth, db } from "@/lib/firebase";
 import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import TweetCard from "@/components/TweetCard";
 
 export default function ProfilePage() {
-
   const [tweets, setTweets] = useState<any>();
   const chatsCollectionRef = collection(db, "chats");
 
   const getTweets = async () => {
-      try {
-        const data = await getDocs(chatsCollectionRef);
-        const filterData = data.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-        // console.log(filterData)
-        setTweets(filterData);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-  
-    useEffect(() => {
-      getTweets();
-    }, []);
+    try {
+      const data = await getDocs(chatsCollectionRef);
+      const filterData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      // console.log(filterData)
+      setTweets(filterData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTweets();
+  }, []);
 
   return (
     <div className="min-h-screen bg-zinc-900 text-white">
@@ -44,45 +39,54 @@ export default function ProfilePage() {
         </Link> */}
 
         <div className="flex flex-col lg:flex-row gap-8">
-          <Sidebar activePage="account" />
+          <Sidebar activePage="account" getTweets={getTweets} />
 
           {/* Main Content */}
-          <div className="flex-1">
-            <h1 className="text-2xl font-bold mb-6">My Account</h1>
+          <div className="flex w-full justify-center">
+            <div className="flex-1 max-w-3xl">
+              <h1 className="text-2xl font-bold mb-6">My Account</h1>
 
-            <Card className="bg-zinc-800 border-none text-white">
-              <CardHeader className="flex flex-row items-center gap-4 py-7">
-                <Avatar className="w-20 h-20">
-                  <AvatarImage
-                    src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-EYceHp4dk9K4DnOYSrlZH3AbFI0wrz.png"
-                    alt="Profile picture"
-                  />
-                  <AvatarFallback>{auth.currentUser?.displayName}</AvatarFallback>
-                </Avatar>
-                <div className="flex-1">
-                  <h2 className="text-xl font-semibold">{auth.currentUser?.displayName}</h2>
-                  <h2 className="text-lg font-medium text-white/60">{auth.currentUser?.email}</h2>
-                  {/* <Button variant="secondary" className="mt-2 bg-indigo-500 hover:bg-indigo-600 text-white">
+              <Card className="bg-zinc-800 border-none text-white">
+                <CardHeader className="flex flex-row items-center gap-4 py-7">
+                  <Avatar className="w-20 h-20">
+                    <AvatarImage
+                      src={auth.currentUser?.photoURL!}
+                      alt="Profile picture"
+                    />
+                    <AvatarFallback>
+                      {auth.currentUser?.displayName}
+                    </AvatarFallback>
+                  </Avatar>
+                  <div className="flex-1">
+                    <h2 className="text-xl font-semibold">
+                      {auth.currentUser?.displayName}
+                    </h2>
+                    <h2 className="text-lg font-medium text-white/60">
+                      {auth.currentUser?.email}
+                    </h2>
+                    {/* <Button variant="secondary" className="mt-2 bg-indigo-500 hover:bg-indigo-600 text-white">
                     Edit User Profile
                   </Button> */}
-                </div>
-              </CardHeader>
-            </Card>
+                  </div>
+                </CardHeader>
+              </Card>
 
-            <h1 className="text-2xl my-3 font-semibold">My Tweets...</h1>
-            <main className="space-y-4">
-            {tweets
-              ? tweets
-                .filter((tw: any) => tw.author === auth.currentUser?.email)
-                .map((tw: any) => (
-                <TweetCard key={tw.id} user={auth} tweet={tw} />
-                ))
-              : "No tweets available!"}
-          </main>
+              <h1 className="text-2xl my-3 font-semibold">My Tweets...</h1>
+              <main className="space-y-4">
+                {tweets
+                  ? tweets
+                      .filter(
+                        (tw: any) => tw.author === auth.currentUser?.email
+                      )
+                      .map((tw: any) => (
+                        <TweetCard key={tw.id} user={auth} tweet={tw} isProfile={true} getTweets={getTweets} />
+                      ))
+                  : "No tweets available!"}
+              </main>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
-
